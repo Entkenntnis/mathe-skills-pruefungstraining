631194459781b8985cb8516ae41687b62972e16c
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-08-20 13:45:26 +0200

    2023 / 3

--- src/content/implementations/by-rs-gw-9-z-i/83-2023-3-gleichwertige-terme.tsx
+++ src/content/implementations/by-rs-gw-9-z-i/83-2023-3-gleichwertige-terme.tsx
@@ -1,13 +1,44 @@
 import { Exercise } from '@/data/types'
 
-interface DATA {}
+interface DATA {
+  randomOrder: number[]
+  options: JSX.Element[]
+  correct: number
+}
 
 export const exercise83: Exercise<DATA> = {
   title: '2023 / 3) Gleichwertige Terme',
   useCalculator: false,
   duration: 1,
   generator(rng) {
-    return {}
+    const correct = rng.randomIntBetween(0, 4)
+    const [a, b] = rng.randomItemFromArray([
+      [2, 3],
+      [3, 4],
+      [2, 4],
+    ])
+    const options: JSX.Element[] = [
+      correct == 0 ? (
+        <>x + x = 2x</>
+      ) : (
+        <>
+          x + x = x<sup>2</sup>
+        </>
+      ),
+      correct == 1 ? <>2x − 4x = −2x</> : <>2 − 4x = −2x</>,
+      correct == 2 ? <>3x − x = 2x</> : <>3x − x = 3</>,
+      correct == 3 ? (
+        <>
+          x<sup>{a}</sup> · x<sup>{b}</sup> = x<sup>{a + b}</sup>
+        </>
+      ) : (
+        <>
+          x<sup>{a}</sup> · x<sup>{b}</sup> = x<sup>{a * b}</sup>
+        </>
+      ),
+      correct == 4 ? <>2x · 3 = 6x</> : <>2x · 3x = 6x</>,
+    ]
+    return { randomOrder: rng.shuffleArray([0, 1, 2, 3, 4]), options, correct }
   },
   constraint({ data }) {
     return true
@@ -20,16 +51,22 @@ export const exercise83: Exercise<DATA> = {
           wahr.
         </p>
         <ol>
-          <li>x + x = x²</li>
-          <li>2 - 4x = -2x</li>
-          <li>3x - 3 = 3</li>
-          <li>x² x³ = x⁵</li>
-          <li>2x 3x = 6x</li>
+          {data.randomOrder.map((i) => (
+            <li key={i}>{data.options[i]}</li>
+          ))}
         </ol>
       </>
     )
   },
   solution({ data }) {
-    return <></>
+    return (
+      <>
+        <p>
+          Nur die{' '}
+          <strong>Aussage {data.randomOrder.indexOf(data.correct) + 1}</strong>{' '}
+          ist immer wahr.
+        </p>
+      </>
+    )
   },
 }
